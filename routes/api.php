eedc9f8ca9510c02f5de6e1c32a8d213377ff002
Diana Dvoryak <dianadvoryak.w@gmail.com>
2024-05-10 12:33:34 +0200

    jwt AuthController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [App\Http\Controllers\AuthController::class, 'me']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
